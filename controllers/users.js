f8288be29d0419619d8f6d5906693556f822eba5
vkostina <v.kostina@city-mobil.ru>
2022-04-20 14:18:56 +0300

    fix users errors by tests

--- controllers/users.js
+++ controllers/users.js
@@ -21,10 +21,10 @@ module.exports.getUserById = (req, res) => {
       name: user.name, about: user.about, avatar: user.avatar, _id: user._id,
       }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(VALIDATION_ERROR_CODE).send({ message: err.message });
+      if (err.name === 'CastError') {
+        return res.status(VALIDATION_ERROR_CODE).send({ message: `Некорректный идентификатор пользователя` });
       }
-      else if (err.name === 'CastError') {
+      else if (err.name === 'TypeError') {
         return res.status(NOT_FOUND_ERR_CODE).send({ message: `Пользователь ${req.params.userId} не существует` });
       }
       return res.status(DEFAULT_ERROR_CODE).send({ message: `Ошибка - ${err}` });
@@ -46,8 +46,8 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send(user))
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => res.send({_id: user._id, name, about}))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(VALIDATION_ERROR_CODE).send({ message: err.message });
@@ -61,8 +61,8 @@ module.exports.updateUser = (req, res) => {
 
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then(() => res.send(user))
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((user) => res.send({_id: user._id, name, about}))
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(NOT_FOUND_ERR_CODE).send({ message: `Пользователь ${req.params.userId} не существует` });

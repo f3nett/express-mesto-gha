cf6f8a315e02061c7e761cd23fd4f6d75c48f7f3
f3nett <f3nett@yandex.ru>
2022-05-15 16:22:16 +0300

    change cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,4 @@
 const SALT_ROUNDS = 10;
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://mesto.f3nett.nomoreparties.sbs',
-  'http://mesto.f3nett.nomoreparties.sbs',
-  'localhost:3000',
-];
 
 const defaultUserName = 'Жак-Ив Кусто';
 const defaultUserAbout = 'Исследователь';
@@ -15,8 +6,6 @@ const defaultUserAvatar = 'https://pictures.s3.yandex.net/resources/jacques-cous
 
 module.exports = {
   SALT_ROUNDS,
-  DEFAULT_ALLOWED_METHODS,
-  allowedCors,
   defaultUserName,
   defaultUserAbout,
   defaultUserAvatar,
